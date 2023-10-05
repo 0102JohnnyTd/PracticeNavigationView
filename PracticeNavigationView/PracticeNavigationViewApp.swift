3a3b28e11f0e01fdf85a8653da374c24cd04c29e
0102johnny <rt2824.recruit.1022@gmail.com>
2023-10-05 19:45:59 +0900

    Initial commit

---
+++ PracticeNavigationView/PracticeNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeNavigationViewApp.swift
+//  PracticeNavigationView
+//
+//  Created by Johnny Toda on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct PracticeNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
